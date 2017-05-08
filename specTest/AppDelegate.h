f37ebe63c16a5352824cb5bee25ef59ae3105d58
liuaaaddxiaoer <oneenddi@163.com>
2017-05-08 15:15:31 +0800

    本地代码创建

---
+++ specTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  specTest
+//
+//  Created by 刘小二 on 2017/5/8.
+//  Copyright © 2017年 刘小二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ specTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  specTest
+//
+//  Created by 刘小二 on 2017/5/8.
+//  Copyright © 2017年 刘小二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
