2e083d82a7a32296a9a0003ca4cc97bdeb728c8e
liuaaaddxiaoer <oneenddi@163.com>
2017-05-08 16:15:06 +0800

    dsdsds

---
+++ specTest/Classes/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  specTest
+//
+//  Created by 刘小二 on 2017/5/8.
+//  Copyright © 2017年 刘小二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ specTest/Classes/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  specTest
+//
+//  Created by 刘小二 on 2017/5/8.
+//  Copyright © 2017年 刘小二. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
